342ab9a606266686ad9cdab7ad53718dcc576575
andrey <a.golchenko@sharksw.com>
2020-11-16 04:15:23 +0200

    made a menu for mobile devices

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,11 +1,10 @@
 //footer navigation
 
-
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.classList === "visible") {
@@ -15,3 +14,41 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+//mobile navigation menu
+
+$(document).ready(function () {
+  $(".burger").click(function (event) {
+    $(
+      ".site-nav__list,.header__background,.site-nav__close"
+    ).toggleClass("site-nav--active");
+  });
+});
+
+$(document).ready(function () {
+  $(".site-nav__close").click(function (event) {
+    $(
+      ".site-nav__list,.header__background,.site-nav__close"
+    ).toggleClass("site-nav--active");
+  });
+});
+
+
+// mobile search
+
+$(document).ready(function () {
+  $(".search").click(function (event) {
+    $(".header__search,.search-close").toggleClass("search--active");
+    $(".header__background").toggleClass("site-nav--active");
+
+  });
+});
+
+$(document).ready(function () {
+  $(".search-close").click(function (event) {
+    $(".header__search,.search-close").toggleClass("search--active");
+    $(".header__background").toggleClass("site-nav--active");
+  });
+});
+
--- build/js/main.js
+++ build/js/main.js
@@ -39,12 +39,11 @@ $(document).ready(function () {
 
 //footer navigation
 
-
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.classList === "visible") {
@@ -54,3 +53,40 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+//mobile navigation menu
+
+$(document).ready(function () {
+  $(".burger").click(function (event) {
+    $(
+      ".site-nav__list,.header__background,.site-nav__close"
+    ).toggleClass("site-nav--active");
+  });
+});
+
+$(document).ready(function () {
+  $(".site-nav__close").click(function (event) {
+    $(
+      ".site-nav__list,.header__background,.site-nav__close"
+    ).toggleClass("site-nav--active");
+  });
+});
+
+
+// mobile search
+
+$(document).ready(function () {
+  $(".search").click(function (event) {
+    $(".header__search,.search-close").toggleClass("search--active");
+    $(".header__background").toggleClass("site-nav--active");
+
+  });
+});
+
+$(document).ready(function () {
+  $(".search-close").click(function (event) {
+    $(".header__search,.search-close").toggleClass("search--active");
+    $(".header__background").toggleClass("site-nav--active");
+  });
+});
